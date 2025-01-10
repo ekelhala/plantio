995125511433c0cd613e0e5e6ed3728af058c635
Emil Kelhälä <emil.kelhala@gmail.com>
2025-01-10 17:27:53 +0200

    Initial commit

---
+++ pico/main.py
@@ -0,0 +1,18 @@
+from machine import Pin, ADC
+from time import sleep
+
+POWER_PIN = 22
+f28PowerPin = Pin(POWER_PIN, Pin.OUT)
+adc = ADC(26)
+
+DRY = 65000
+WET = 47000
+
+while True:
+    f28PowerPin.on()
+    sleep(1)
+    moisture = adc.read_u16()
+    f28PowerPin.off()
+    moistureLevel = max(0, min(100, 100 * (DRY - moisture) / (DRY - WET)))
+    print("{:.2f} %".format(moistureLevel))
+    sleep(10)
