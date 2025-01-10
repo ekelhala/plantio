e6c5d01c6a7531ac22649e14c7cc7e32a0ec73ff
Emil Kelhälä <emil.kelhala@gmail.com>
2025-01-10 17:34:37 +0200

    Naming changes

--- pico/main.py
+++ pico/main.py
@@ -2,17 +2,17 @@ from machine import Pin, ADC
 from time import sleep
 
 POWER_PIN = 22
-f28PowerPin = Pin(POWER_PIN, Pin.OUT)
+fc28PowerPin = Pin(POWER_PIN, Pin.OUT)
 adc = ADC(26)
 
 DRY = 65000
 WET = 47000
 
 while True:
-    f28PowerPin.on()
+    fc28PowerPin.on()
     sleep(1)
     moisture = adc.read_u16()
-    f28PowerPin.off()
+    fc28PowerPin.off()
     moistureLevel = max(0, min(100, 100 * (DRY - moisture) / (DRY - WET)))
     print("{:.2f} %".format(moistureLevel))
     sleep(10)
