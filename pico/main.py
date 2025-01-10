30223e0f5ddc319b0ded63c18d27864bbce47855
Emil Kelhälä <emil.kelhala@gmail.com>
2025-01-10 18:13:41 +0200

    Add MQTT, config and gitignore

--- pico/main.py
+++ pico/main.py
@@ -1,5 +1,9 @@
 from machine import Pin, ADC
 from time import sleep
+from umqtt.simple import MQTTClient
+import ssl
+import network
+import config
 
 POWER_PIN = 22
 fc28PowerPin = Pin(POWER_PIN, Pin.OUT)
@@ -7,12 +11,55 @@ adc = ADC(26)
 
 DRY = 65000
 WET = 47000
+MINUTE = 60 # minute in seconds
 
-while True:
+MOISTURE_LEVEL = 'moistureLevel'
+
+state = {
+    'moistureLevel': 0
+}
+
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+wlan.connect(config.ssid, config.pwd)
+
+connection_timeout = 10
+while connection_timeout > 0:
+    if wlan.status() == 3: # connected/ip obtained
+        break
+    connection_timeout -= 1
+    print('Waiting for Wi-Fi connection...')
+    sleep(1)
+
+# check if connection successful
+if wlan.status() != 3: 
+    raise RuntimeError('[ERROR] Failed to establish a network connection')
+else:
+    print('[INFO] CONNECTED SUCCESSFULLY')
+
+sslContext = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+sslContext.verify_mode = ssl.CERT_NONE
+
+mqttClient = MQTTClient(client_id=config.MQTT_USER, server=config.MQTT_BROKER, port=config.MQTT_PORT,
+                    user=config.MQTT_USER, password=config.MQTT_PWD, ssl=sslContext)
+mqttClient.connect()
+print('MQTT client connected')
+
+def measure():
     fc28PowerPin.on()
     sleep(1)
     moisture = adc.read_u16()
     fc28PowerPin.off()
     moistureLevel = max(0, min(100, 100 * (DRY - moisture) / (DRY - WET)))
-    print("{:.2f} %".format(moistureLevel))
-    sleep(10)
+    state[MOISTURE_LEVEL] = round(moistureLevel)
+    print("{}%".format(moistureLevel))
+    
+def publish():
+    mqttClient.publish('/moisture_level', str(state[MOISTURE_LEVEL]))
+    print('published')
+    
+while True:
+    measure()
+    print(state[MOISTURE_LEVEL])
+    publish()
+    sleep(5*MINUTE) # sleeping for 5 minutes
