eb2228c5ad04e10411e0d88dc71e281f70bcb947
Emil Kelhälä <emil.kelhala@gmail.com>
2025-01-11 16:13:51 +0200

    Add dotenv, make MQTT connection work

--- collector/src/index.ts
+++ collector/src/index.ts
@@ -1,1 +1,39 @@
+import mqtt, { MqttClient } from "mqtt"
+import { configDotenv } from "dotenv"
+configDotenv()
+
+let mqttClient : MqttClient
+const mqttOptions = {
+    port: Number(process.env.MQTT_PORT),
+    username: process.env.MQTT_USERNAME,
+    password: process.env.MQTT_PASSWORD}
+
+const topics = ['/moisture_level']
+
+const onMQTTMessage = async (topic: string, message: Buffer) => {
+        try {
+            const messageParsed = JSON.parse(message.toString())
+            console.log(messageParsed)
+        }
+        catch(error) {
+            console.log('Invalid message received:', message.toString())
+        }
+}
+
+const init = async () => {
+    // initialize MQTT
+    try {
+        mqttClient = await mqtt.connectAsync(process.env.MQTT_URL, mqttOptions)
+        console.log('Connected to MQTT broker')
+        mqttClient.subscribe(topics)
+        mqttClient.on('message', onMQTTMessage)
+        console.log('MQTT successfully initialized')
+    }
+    catch(err) {
+        console.log('Unable to connect to MQTT broker, got error: ', err)
+    }
+    // initialize MongoDB
+}
+
 console.log('Server is starting')
+init()
