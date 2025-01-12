5b77911532b67b0b6ff729493cf4542ea3a9d0e3
Emil Kelhälä <emil.kelhala@gmail.com>
2025-01-12 11:06:19 +0200

    Added tsconfig and initialization function

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,27 @@
 import Express from 'express'
 import { configDotenv } from 'dotenv'
 configDotenv()
+import mongoose from 'mongoose'
 
 import api from './routes/api'
 
 const PORT = Number(process.env.PORT) || 8000
 
+const init = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URL)
+        console.log('Connected to MongoDB')
+    }
+    catch(err) {
+        console.log('Failed to connect to MongoDB, got error:', err)
+    }
+}
+
 const app = Express()
 
 app.use('/api', api)
 
+init()
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`)
 })
