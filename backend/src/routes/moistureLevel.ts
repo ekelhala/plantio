7437792cc4d545f556f20871346ced68150155e1
Emil Kelhälä <emil.kelhala@gmail.com>
2025-01-12 16:15:13 +0200

    Implement moisture level queries from API by nodeId

--- backend/src/routes/moistureLevel.ts
+++ backend/src/routes/moistureLevel.ts
@@ -1,9 +1,20 @@
 import { Router } from "express";
+import { MoistureLevel } from "../models/MoistureLevel";
 
 const router = Router()
 
-router.get('/:id', (req, res) => {
-    
+router.get('/:nodeId', async(req, res) => {
+    try {
+        const latestMoistureLevel = await MoistureLevel.aggregate([
+                {$match: {nodeId: req.params.nodeId}},
+                {$sort: {createdAt: -1}},
+                {$limit: 1}
+            ])
+        res.json(latestMoistureLevel)
+    }
+    catch(error) {
+        res.status(400).json({error: 'Invalid node id'})
+    }
 })
 
 export default router
