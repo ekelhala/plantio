d08a3b60ae952e6b44b586ba2d06c8e6ba2bd7c3
Emil Kelhälä <emil.kelhala@gmail.com>
2025-01-12 16:34:22 +0200

    Fix latest moisture level query by node id

--- backend/src/routes/moistureLevel.ts
+++ backend/src/routes/moistureLevel.ts
@@ -5,14 +5,14 @@ const router = Router()
 
 router.get('/:nodeId', async(req, res) => {
     try {
-        const latestMoistureLevel = await MoistureLevel.aggregate([
-                {$match: {nodeId: req.params.nodeId}},
-                {$sort: {createdAt: -1}},
-                {$limit: 1}
-            ])
+        // find one moisture level associated to requested node id, sort timestamps in descending
+        // order to get the latest
+        const latestMoistureLevel = await MoistureLevel.findOne({nodeId: req.params.nodeId})
+                                                        .sort({timestamp: -1})
         res.json(latestMoistureLevel)
     }
     catch(error) {
+        console.log(error)
         res.status(400).json({error: 'Invalid node id'})
     }
 })
