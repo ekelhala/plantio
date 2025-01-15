e9a5534ae038fadb5e6a12046c5c3d33ae17442b
Emil Kelhälä <emil.kelhala@gmail.com>
2025-01-15 20:01:58 +0200

    Add appbar to Home screen

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,6 +1,7 @@
-import { useEffect, useState } from "react"
+import { FormEvent, useEffect, useState } from "react"
 import User from "../types/User"
 import { getNodes } from "../services/nodes"
+import { AppBar, Avatar, IconButton, Menu, MenuItem, Toolbar } from "@mui/material"
 
 interface HomeProps {
     user: User
@@ -15,6 +16,8 @@ interface NodeInfo {
 const Home = (props: React.PropsWithoutRef<HomeProps>) => {
 
     const [nodeInfos, setNodeInfos] = useState<NodeInfo[]|null>(null)
+    const [menuOpen, setMenuOpen] = useState<boolean>(false)
+    const [menuAnchor, setMenuAnchor] = useState<HTMLElement|null>(null)
 
     useEffect(() => {
         const effect = async () => {
@@ -24,9 +27,35 @@ const Home = (props: React.PropsWithoutRef<HomeProps>) => {
         effect()
     }, [])
 
+    const onMenuOpen = (event: React.MouseEvent<HTMLButtonElement>) => {
+        setMenuAnchor(event.currentTarget)
+    }
+
     return(
         <>
-            {props.user.name}
+            <AppBar position='fixed'>
+                <Toolbar>
+                    <div style={{display: 'flex', justifyContent: 'flex-end', width: '100%'}}>
+                        <IconButton
+                            onClick={(event) => onMenuOpen(event)}>
+                            <Avatar>
+                                {props.user.name.charAt(0)}
+                            </Avatar>
+                        </IconButton>
+                        <Menu
+                            anchorEl={menuAnchor}
+                            anchorOrigin={{
+                                vertical: 'center',
+                                horizontal: 'left',
+                              }}
+                            open={(menuAnchor !== null)}
+                            onClose={() => setMenuAnchor(null)}>
+                            <MenuItem>Log out</MenuItem>
+                        </Menu>
+                    </div>
+                </Toolbar>
+            </AppBar>
+            
             {nodeInfos ? nodeInfos.map(nodeInfo => {
                 return (
                 <div>
