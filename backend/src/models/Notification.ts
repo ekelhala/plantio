c0bdfe69ba66d056a760b97958baf137eb84a38e
Emil Kelhälä <emil.kelhala@gmail.com>
2025-01-17 19:20:56 +0200

    Add JSON transform to notification model, add client notification service

--- backend/src/models/Notification.ts
+++ backend/src/models/Notification.ts
@@ -4,6 +4,16 @@ const notificationSchema = new Schema({
     percentage: Number,
     nodeId: String,
     userId: String
+},
+{
+    toJSON: {
+      transform: (doc, ret) => {
+        ret.id = ret._id
+        delete ret._id
+        delete ret.__v
+        return ret
+      },
+    },
 })
 
 export const Notification = mongoose.model('Notification', notificationSchema)
--- frontend/src/services/notifications.ts
+++ frontend/src/services/notifications.ts
@@ -3,3 +3,7 @@ import axios from "axios";
 export const API_URL: string = import.meta.env.DEV ? 'http://localhost:8000' : '/api'
 
 const BASE_URL = `${API_URL}/notifications`
+
+export const createNotification = async (nodeId: string, percentage: number) => await axios.post(BASE_URL, {nodeId, percentage})
+export const getNotifications = async () => await axios.get(BASE_URL)
+export const deleteNotification = async (id: string) => await axios.delete(`${BASE_URL}/${id}`)
