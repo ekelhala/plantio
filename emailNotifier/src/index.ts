7e9b0b87e9a075511d70fdaab81d1c242cdb7163
Emil Kelhälä <emil.kelhala@gmail.com>
2025-01-19 12:07:31 +0200

    New fix for email service

--- emailNotifier/src/index.ts
+++ emailNotifier/src/index.ts
@@ -5,8 +5,8 @@ import mongoose from "mongoose";
 configDotenv()
 
 mongoose.connect(process.env.MONGODB_URL)
-    .then(() => {
-        consumeMessages('moisture_level', async (message) => {
+    .then(async () => {
+        await consumeMessages('moisture_level', async (message) => {
             await processMoistureMessage(message)
         })
         console.log('server started')
